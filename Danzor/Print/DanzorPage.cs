5d42e60dd39435d986babfebf252b9325864f946
Yan Justino <yanlimaj@gmail.com>
2014-12-05 12:48:21 -0300

    page

---
+++ Danzor/Print/DanzorPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Danzor.Print
+{
+    public class DanzorPage
+    {
+        public int page { get; set; }
+        public int size { get; set; }
+        public DanzorHeader Header { get; set; }
+        public DanzorBody Body { get; set; }
+        public DanzorFooter Footer { get; set; }
+        public bool IsFirstPage
+        {
+            get
+            {
+                return this.page.Equals(1);
+            }
+        }
+        public bool IsLastPage
+        {
+            get
+            {
+                return this.page.Equals(size);
+            }
+        }
+
+        public DanzorPage(dynamic nfe, dynamic protNFe, int page, int size)
+        {
+            this.page = page;
+            this.size = size;
+
+            this.Header = new DanzorHeader(nfe);
+            this.Body = new DanzorBody(nfe, protNFe, page, size);
+            this.Footer = new DanzorFooter(nfe);
+        }
+    }
+}
